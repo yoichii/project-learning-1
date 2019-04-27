2cab50a6f7ddddf1b4c9c8241870e501a90fc94f
yoichi <40388939+yoichii@users.noreply.github.com>
2019-04-28 00:05:53 +0900

    pieces[] to boardNum[][] in PlayScreen

--- PlayScreen.java
+++ PlayScreen.java
@@ -6,7 +6,7 @@ import javax.swing.border.*;
 public class PlayScreen extends BaseScreen {
 	private static final long serialVersionUID = 1L;
 
-    private int pieces[] = new int[64];
+    private int boardNum[][] = new int[8][8];
     private JButton board[][] = new JButton[8][8];
     private Controller controller;
 
@@ -24,33 +24,38 @@ public class PlayScreen extends BaseScreen {
         JPanel boardPanel = new JPanel();
         boardPanel.setPreferredSize(new Dimension(300, 300));
         boardPanel.setLayout(new GridLayout(8, 8));
-        // pieces init
-        for(int i = 0; i < 64; ++i) {
-            pieces[i] = 0;
-            if(i == 27 || i == 36) {
-                pieces[i] = 1;
-            } else if( i == 28 || i == 35) {
-                pieces[i] = 2;
+        // boardNum init
+        for(int i = 0; i < 8; ++i) {
+            for(int j = 0; j < 8; ++j) {
+                if( (i == 3 && j == 3) || (i == 4 && j == 4) )
+                    boardNum[i][j] = 1;
+                else if( (i == 4 && j == 3) || (i == 3 && j == 4) )
+                    boardNum[i][j] = 2;
+                else
+                    boardNum[i][j] = 0;
             }
         }
+
         // instanciate buttons
-        for(int i = 0; i < pieces.length; ++i) {
-            JButton square = new JButton();
-            square.setBorder(new LineBorder(Color.black, 1, false));
-            board[i/8][i%8] = square;
+        for(int i = 0; i < boardNum.length; ++i) {
+            for(int j = 0; j < boardNum[0].length; ++j) {
+                JButton square = new JButton();
+                square.setBorder(new LineBorder(Color.black, 1, false));
+                board[i][j] = square;
 
-            if (pieces[i] == 0) {
-                square.setIcon(new ImageIcon("images/green.png"));
-                square.setOpaque(true);
-            } else if(pieces[i] == 1) {
-                square.setIcon(new ImageIcon("images/black.png"));
-                square.setOpaque(true);
-            } else {
-                square.setIcon(new ImageIcon("images/white.png"));
-                square.setOpaque(true);
+                if (boardNum[i][j] == 0) {
+                    square.setIcon(new ImageIcon("images/green.png"));
+                    square.setOpaque(true);
+                } else if(boardNum[i][j] == 1) {
+                    square.setIcon(new ImageIcon("images/black.png"));
+                    square.setOpaque(true);
+                } else {
+                    square.setIcon(new ImageIcon("images/white.png"));
+                    square.setOpaque(true);
+                }
+
+                boardPanel.add(square);
             }
-
-            boardPanel.add(square);
         }
 
         // resign button
