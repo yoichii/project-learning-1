4a9687b077e4c9050b6c905d511a1f01bbd1e001
yoichi <40388939+yoichii@users.noreply.github.com>
2019-05-22 16:06:07 +0900

    add background animation

--- ImagePanel.java
+++ ImagePanel.java
@@ -1,12 +1,17 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 class ImagePanel extends JPanel {
 
     private Image img;
+    private Image fuji;
+    private int i = 0;
 
     public ImagePanel() {
         this(new ImageIcon("images/background.jpg").getImage());
+        fuji = new ImageIcon("images/fuji.png").getImage();
+
     }
 
     public ImagePanel(String img) {
@@ -15,10 +20,31 @@ class ImagePanel extends JPanel {
 
     public ImagePanel(Image img) {
         this.img = img;
+
+
+        // timer
+        ActionListener listener = new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                if(i < 30)
+                    i += 1;
+                else
+                    ((Timer)event.getSource()).stop();
+
+                repaint();
+            }
+        };
+
+        Timer timer = new Timer(30, listener);
+        timer.setRepeats(true);
+        timer.setInitialDelay(0);
+        timer.start();
+
     }
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(img, 0, 0, null);
+        //g.drawImage(fuji, 0, 0, null);
+        g.drawImage(fuji, 0, 1100 - (int)(800*Math.sin(i*Math.PI/60)), null);
     }
 
 }
